a01436119a901f2db5d5b3171036a5cf90b22d56
하늘의 한계 <qeoseol@gmail.com>
2025-02-16 02:51:37 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ task.wait(15)
 ------------------------------------------------------
  
 print('- queueing code')
-queue_on_teleport('loadstring(game:HttpGet("https://pastebin.com/raw/vRESX1Zq", true))()')
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/kagehana/aam/refs/heads/main/ingame.lua", true))()')
  
 print('- matchmaking')
 game:GetService('ReplicatedStorage').endpoints.client_to_server.request_matchmaking:InvokeServer('christmas_event')
